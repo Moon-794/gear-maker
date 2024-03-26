ac3f0bfb7e06c4825ee3966f376a494e66c11201
Moon-794 <moon0795@outlook.com>
2024-03-26 04:57:41 +0000

    Single segment working

--- meshgen.js
+++ meshgen.js
@@ -32,10 +32,99 @@ function GenerateCircle(divisions)
     //Center point, index 0
     AddVertexPosition(vertices, 0, 0, 0);
 
-    var x = Math.sin(0);
-    var y = Math.cos(0);
+    //Order of operations:
+    //Loop through a number of circular segments, each will draw 2 tooth halves
+    var module = 0.5;
+    var numTeeth = 8;
+    var topWidth = 0.2;
+    var pitchCircleRadius = (module * numTeeth) / 2;
+    var addendum = pitchCircleRadius + module;
+
+    var toothSegAngle = (2 * 3.14159) / numTeeth;
+    var singleToothAngle = toothSegAngle / 3;
+
+    //First bit of top land
+    var x = Math.sin(0) * addendum;
+    var y = Math.cos(0) * addendum;
     var z = 0;
     AddVertexPosition(vertices, x, y, z);
+
+    //Second bit
+    //Slope of current tangent line of the root radius
+    var m = -x/y;
+
+    var x2 = x + topWidth/Math.sqrt(1+Math.pow(m, 2));
+    var y2 = y + m * (topWidth/Math.sqrt(1+Math.pow(m, 2)));
+    var z2 = 0;
+
+    AddVertexPosition(vertices, x2, y2, z2);
+
+    indices.push(0);
+    indices.push(1);
+    indices.push(2);
+
+    var x3 = Math.sin(singleToothAngle) * pitchCircleRadius;
+    var y3 = Math.cos(singleToothAngle) * pitchCircleRadius;
+    var z3 = 0;
+
+    AddVertexPosition(vertices, x3, y3, z3);
+
+    indices.push(0);
+    indices.push(2);
+    indices.push(3);
+
+    var base = 3;
+    for (let q = 0; q < 3; q++) 
+    {
+        var phi = singleToothAngle / 3;
+        var x = Math.sin(singleToothAngle + ((q + 1) * phi)) * pitchCircleRadius;
+        var y = Math.cos(singleToothAngle + ((q + 1) * phi)) * pitchCircleRadius;
+        var z = 0;
+        
+        AddVertexPosition(vertices, x, y, z);
+        
+        var i = (indices.length / 3) + 1;
+        indices.push(0);
+        indices.push(i);
+        indices.push(i + 1);
+    }
+
+    var x4 = Math.sin(toothSegAngle) * addendum;
+    var y4 = Math.cos(toothSegAngle) * addendum;
+    var z4 = 0;
+
+    var mFT = -x4/y4;
+
+    var x5 = x4 - topWidth/Math.sqrt(1+Math.pow(mFT, 2));
+    var y5 = y4 - mFT * (topWidth/Math.sqrt(1+Math.pow(mFT, 2)));
+    var z5 = 0; 
+
+    AddVertexPosition(vertices, x5, y5, z5);
+    var i = (indices.length / 3) + 1;
+    indices.push(0);
+    indices.push(i);
+    indices.push(i + 1);
+
+    AddVertexPosition(vertices, x4, y4, z4);
+    var i = (indices.length / 3) + 1;
+    indices.push(0);
+    indices.push(i);
+    indices.push(i + 1);
+
+    /*
+    for (let toothNo = 0; toothNo < numTeeth; toothNo++) 
+    {
+        //First, draw half of the top land of the tooth
+        var phi = toothNo * toothSegAngle;
+        var tlX = Math.sin(phi);
+        var tlY = Mathf.cos(phi);
+
+        AddVertexPosition(vertices, tlX, tlY, 0);
+
+        indices.push(0);
+        indices.push(i);
+        indices.push(i + 1);
+    }
 
     for (let i = 1; i < divisions; i++) 
     {
@@ -52,6 +141,7 @@ function GenerateCircle(divisions)
     indices.push(0);
     indices.push(divisions);
     indices.push(1);
+    */
     
     const finalVertexArray = new Float32Array(vertices);
 	geometry.setIndex(indices);
